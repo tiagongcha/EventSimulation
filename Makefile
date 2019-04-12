25781287d60c70485d5fffd8d9b1fac94c3ab967
rtkaratekid <42547811+rtkaratekid@users.noreply.github.com>
2019-04-12 11:23:11 -0600

    refactoring (#1)

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
+refactor:
+	clang++ -c -std=c++17 main.cpp bank.cpp market.cpp
+	clang++ -o RefactoredDES main.o bank.cpp market.cpp
+
 simulate: simulate.o main.o
 	clang++ -c main.cpp
 	clang++ -c simulate.cpp
 	clang++ -o simulate simulate.o main.o
-
+clean:
+	rm -f main.o bank.o market.o RefactoredDES
 
 
 
--- main.cpp
+++ main.cpp
@@ -8,29 +8,42 @@
 
 #include <iostream>
 #include <string>
-#include "simulate.hpp"
+//#include "simulate.hpp"
+#include "bank.h"
+#include "market.h"
+//#include "checker.h"
+//#include "event.h"
 
 
 
 int main(int argc, const char * argv[]) {
-    if (argc < 4) {
-        exit(1);
-    }
+//    if (argc < 4) {
+//        exit(1);
+//    }
 //
     int simulationDuration = 43200;
-    double arrivalRate = std::stod(argv[1]);
-////    std::cout << arrivalRate << std::endl;
-    int totalCustomer = (int)(arrivalRate * 60 * 12);
-////    std::cout <<  totalCustomer << std::endl;
-    double serviceRange = std::stod(argv[2]);
-////     std::cout << serviceRange << std::endl;
-    double serviceTime = serviceRange * 60;
+//    double arrivalRate = std::stod(argv[1]);
+//    int totalCustomer = (int)(arrivalRate * 60 * 12);
+//    double serviceRange = std::stod(argv[2]);
+//    double serviceTime = serviceRange * 60;
+//    int seed = std::stoi(argv[3]);
+
+    int arrivalRate = 10;
+    int totalCustomer = 10;
+    double serviceRange = 10;
+    double serviceTime = 10;
+    int seed = 34567;
+
+//    std::cout << arrivalRate << std::endl;
+//    std::cout <<  totalCustomer << std::endl;
+//    std::cout << serviceRange << std::endl;
 //    std::cout << "service time " <<serviceTime << std::endl;
-    int seed = std::stoi(argv[3]);
-////     std::cout << seed << std::endl;
+//    std::cout << seed << std::endl;
+
+
     std::cout << "the arrival rate is " << arrivalRate << std::endl;
     std::cout << "super market stat is " << std::endl;
-    WholeFood wholefood;
+    market wholefood;
     wholefood.populateEvent(seed, simulationDuration, serviceTime, totalCustomer);
     wholefood.simulate();
     wholefood.getStat();
